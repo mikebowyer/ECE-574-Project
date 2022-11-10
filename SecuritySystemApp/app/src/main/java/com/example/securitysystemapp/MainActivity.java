eb03120091ce2c45053996b869bc7a24785c4e68
Mike Bowyer <mbowyer@umich.edu>
2022-11-09 21:33:49 -0500

    Working socket connection

--- SecuritySystemApp/app/src/main/java/com/example/securitysystemapp/MainActivity.java
+++ SecuritySystemApp/app/src/main/java/com/example/securitysystemapp/MainActivity.java
@@ -14,9 +14,20 @@ import android.view.View;
 import com.example.securitysystemapp.ui.main.SectionsPagerAdapter;
 import com.example.securitysystemapp.databinding.ActivityMainBinding;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+
+    // Socket Info
+    private Socket socket;
+    private static final int SERVERPORT = 5000;
+    private static final String SERVER_IP = "DESKTOP-9MIF63P";
+//    private static final String SERVER_IP = "192.168.0.109";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,10 +36,33 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        // Setup Page Adapter and Tabs
         SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
         ViewPager viewPager = binding.viewPager;
         viewPager.setAdapter(sectionsPagerAdapter);
         TabLayout tabs = binding.tabs;
         tabs.setupWithViewPager(viewPager);
+
+        new Thread(new ClientThread()).start();
+    }
+
+    class ClientThread implements Runnable {
+
+        @Override
+        public void run() {
+
+            try {
+                InetAddress serverAddr = InetAddress.getByName(SERVER_IP);
+
+                socket = new Socket(serverAddr, SERVERPORT);
+
+            } catch (UnknownHostException e1) {
+                e1.printStackTrace();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+
+        }
+
     }
 }
