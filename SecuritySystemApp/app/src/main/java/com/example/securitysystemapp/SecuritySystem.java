85bfe3f4a24c9696ec5437ab9151fade08ffa0f0
Mike Bowyer <mbowyer@umich.edu>
2022-11-15 18:41:42 -0500

    Commiting work so far

--- SecuritySystemApp/app/src/main/java/com/example/securitysystemapp/SecuritySystem.java
+++ SecuritySystemApp/app/src/main/java/com/example/securitysystemapp/SecuritySystem.java
@@ -3,6 +3,7 @@ package com.example.securitysystemapp;
 import android.util.Log;
 
 public class SecuritySystem {
+    public int expected_message_length = 26; // Number of characters in message string
     public int lights = -1;
     public int light_on_hour = -1;
     public int light_on_min = -1;
@@ -22,5 +23,26 @@ public class SecuritySystem {
     public void setStateWithReceivedPacket(String message)
     {
         Log.i("SecuritySystem","Parsing input message");
+        if( message.length() != expected_message_length)
+        {
+            Log.e("SecuritySystem","not correct length");
+        }
+        else
+        {
+            parseMessageUpdateState(message);
+        }
+    }
+
+    public void parseMessageUpdateState(String message)
+    {
+        Character enable_byte_msb = message.charAt(0);
+        Character enable_byte_lsb = message.charAt(1);
+        int enable_byte_msb_int = enable_byte_msb.digit(enable_byte_msb,16);
+        String binaryString = Integer.toBinaryString(enable_byte_msb_int);
+        byte bitMask = (byte)(0x8<<(byte)enable_byte_msb_int);
+        Log.i("Test","feck");
+
+
+
     }
 }
